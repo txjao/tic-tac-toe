dbf4896eaebc9f7bd96c66eb39389f7fd7fe1d36
João <jaobacanao@gmail.com>
2023-06-26 23:02:36 -0300

    styling button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,11 +4,11 @@ interface ButtonProps {
 }
 
 export function Button(props: ButtonProps) {
-    
+
 
     return (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={props.onClick}>
-
+        <button className="bg-cyan-200 h-12 w-auto px-1 border-4 border-black rounded-md  shadow-[-2px_3px_0_0_rgba(0,0,0)] cursor-pointer hover:bg-cyan-50 hover:transition-colors hover:relative bottom-0.5" onClick={props.onClick}>
+                1 Jogador
         </button>
     );
 }
